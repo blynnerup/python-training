555a90720d6dc69710b1cb68c9eae1aab248a9e2
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-07-28 09:44:20 +0200

    Adds example of unpaacking

--- Functions/args.py
+++ Functions/args.py
@@ -3,3 +3,14 @@ def contains_purple(*args):
 
 print(contains_purple(25, "purple"))
 print(contains_purple("green", False, 37, "blue", "hello world"))
+
+# Unpacking of args
+def sum_all_values(*args):
+    print(args)
+    total = 0
+    for num in args:
+        total += num
+    print(total)
+
+nums = (1,2,3,4,5,6)
+sum_all_values(*nums)
