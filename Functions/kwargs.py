bb0c717a4c0b7a223060fbcff43d0c810b983914
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-07-28 11:48:10 +0200

    Nightmare exercise

--- Functions/kwargs.py
+++ Functions/kwargs.py
@@ -16,3 +16,24 @@ def display_names(first, second):
 
 names = {"first": "Colt", "second": "Rusty"}
 display_names(**names)
+
+
+#Exercise
+def calculate(**kwargs):
+    operation_lookup = {
+        'add': kwargs.get('first', 0) + kwargs.get('second', 0),
+        'subtract': kwargs.get('first', 0) - kwargs.get('second', 0),
+        'divide': kwargs.get('first', 0) / kwargs.get('second', 0),
+        'multiply': kwargs.get('first', 0) * kwargs.get('second', 0)
+    }
+    is_float = kwargs.get('make_float', False)
+    operation_value = operation_lookup[kwargs.get('operation', '')]
+    if is_float:
+        final = f"{kwargs.get('message','The result is')} {float(operation_value)}"
+    else:
+        final = f"{kwargs.get('message','The result is')} {int(operation_value)}"
+    return final
+    return "The result is " + str(sum)
+
+print(calculate(make_float=False, operation='add', message='You just added', first=2, second=4))
+print(calculate(make_float=True, operation='divide', first=3.5, second=5))
