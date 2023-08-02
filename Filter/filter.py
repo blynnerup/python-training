dcf26efce540a3cd1aa1f718dae71aa2eb794934
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-02 20:14:36 +0200

    Adds more filter exercises

--- Filter/filter.py
+++ Filter/filter.py
@@ -2,3 +2,8 @@ def remove_negatives (ls):
     return list(filter(lambda num: num > -1, ls))
 
 print(remove_negatives([1,3,-5,7,-9, 0]))
+
+def triple_and_filter(ls):
+    return [x * 3 for x in list(filter(lambda x: x % 4 == 0,ls))]
+
+print(triple_and_filter([1,2,3,4]))
