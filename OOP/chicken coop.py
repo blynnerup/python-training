f05da6d85318d3141ef82438b79ef8cfa1d9fcc5
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-09 10:34:55 +0200

    Adds the chicken coop exercise

---
+++ OOP/chicken coop.py
@@ -0,0 +1,22 @@
+class Chicken:
+    total_eggs = 0
+
+    def __init__(self, species, name, eggs = 0):
+        self.species = species
+        self.name = name
+        self.eggs = eggs
+
+    def lay_egg(self):
+        self.eggs += 1
+        Chicken.total_eggs += 1
+
+    
+c1 = Chicken(name="Alice", species="Partridge Silkie")
+c2 = Chicken(name="Amelia", species="Speckled Sussex")
+
+c1.lay_egg()
+c2.lay_egg() 
+c2.lay_egg()
+
+print(Chicken.total_eggs)
+    
