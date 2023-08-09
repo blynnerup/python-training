f3991d1f41782b3cb2e6d042a41101d45f94c777
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-09 11:49:04 +0200

    Adds methods to deck
    Creates the logic that enables dealing cards and shufling the deck.

--- Deck of Cards/deck.py
+++ Deck of Cards/deck.py
@@ -1,19 +1,50 @@
+import random
+from card import Card
+
 class Deck:
     number_of_cards_in_new_deck = 52
     current_deck = []
 
-    def __init__(self) -> None:
-        pass
-
+    def __init__(self):
+        suits = ['♠', '♦', '♥', '♣']
+        values =[num for num in range(2,11)]
+        face_value = ['J',"K","Q","A"]
+        values.extend(face_value)
+        self.cards = [Card(value, suit) for value in values for suit in suits]
+        
     def __repr__(self) -> str:
-        return f"Deck of {len(Deck.current_deck)} cards."
-    
-    def _deal(self, number_to_deal):
-        try:
-            return [c for c in range(self.number_to_deal, self.current_deck)]
-        except ValueError:
-            return "All cards have been dealt"
+        return f"Deck of {len(self.cards)} cards."   
 
     def count(self):
-        return len(Deck.current_deck)
+        return len(self.cards)
     
+    def _deal(self, deal_cards):
+        try:
+            hand = self.cards[-deal_cards:]
+            self.cards = self.cards[:-deal_cards]
+        except ValueError:
+            return "All cards have been dealt"
+        return hand
+    
+    def deal_card(self):
+        return self._deal(1)
+    
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+    
+    def shuffle(self):
+        if len(self.cards) == 52:
+            random.shuffle(self.cards)
+        else:
+            raise ValueError("Only full decks can be shuffled.")
+                           
+    
+d = Deck()
+d.shuffle()
+print(d.count())
+print(d)
+print(d.deal_card())
+print(d)
+print(d.deal_hand(5))
+print(d)
+# d.shuffle()
