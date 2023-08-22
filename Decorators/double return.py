452a54eaf6af604956b9b1077255cec34d4e737e
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-22 06:37:49 +0200

    Adds args exercise
    Will only return a result if the args are no longer than two.

--- Decorators/double return.py
+++ Decorators/double return.py
@@ -17,3 +17,19 @@ def greet(name):
     return "Hi, I'm " + name
 
 greet("Colt")
+
+
+def ensure_fewer_than_three_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if len(args) > 2:
+            return "Too many arguments!"
+        else:
+            return fn(*args, **kwargs)
+    return wrapper
+
+def add_all(*nums):
+    return sum(nums)
+
+add_all(1)
+add_all(1,2)
