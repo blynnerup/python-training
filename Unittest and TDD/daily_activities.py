396dc53748b847065b49c21ec70a7782ad011436
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-26 08:53:56 +0200

    Wrote logic to comply with tests.
    In TDD terms we went from red to green and now know that our logic works as intended with the tests we wrote.

--- Unittest and TDD/daily_activities.py
+++ Unittest and TDD/daily_activities.py
@@ -1,5 +1,10 @@
 def work(hour):
-    pass
+    if hour >= 8 and hour < 16:
+        return True
+    return False
 
 def can_take_nap(hour):
-    pass
+    if hour > 15 and hour < 20:
+       return "Ok, I can take a nap."
+    return "I will not nap at this time, have other things to do."
+
