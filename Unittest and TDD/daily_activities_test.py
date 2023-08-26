3f4894a6be2e09f41f1fed7f5a6998479cdf10fd
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-08-26 08:49:21 +0200

    Creates some test for TDD

--- Unittest and TDD/daily_activities_test.py
+++ Unittest and TDD/daily_activities_test.py
@@ -2,7 +2,23 @@ import unittest
 from daily_activities import work, can_take_nap
 
 class TestDailyActivities(unittest.TestCase):
-    pass
+    def test_should_be_working(self):
+        self.assertEqual(
+            work(9),
+            True
+        )
+
+    def test_should_not_be_working(self):
+        self.assertEqual(
+            work(18),
+            False
+        )
+
+    def test_can_nap(self):
+        self.assertEqual(
+            can_take_nap(17),
+            'Ok, I can take a nap.'             
+        )
 
 if __name__ == "__main__":
-    unittest.main
+    unittest.main()
