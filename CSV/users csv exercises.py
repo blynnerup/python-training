efdd47c9124a47385bb01edde01f0f824eafb18f
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-09-01 07:47:53 +0200

    Adds logic to print contents of the users.csv file
    Also deletes a file created by accident as the py file was executed in the wrong folder.

--- CSV/users csv exercises.py
+++ CSV/users csv exercises.py
@@ -1,4 +1,5 @@
 '''
+Ex1:
 For this exercise, you'll be working with a file called users.csv.
 Each row of data consists of two columns: a user's first name, and a user's last name.
 
@@ -12,7 +13,7 @@ add_user("Dwayne", "Johnson") # None
 # Colt,Steele
 # Dwayne,Johnson
 '''
-from csv import writer
+from csv import writer, DictReader
 
 def add_user(first_name, last_name):
     with open("users.csv", encoding="utf8", mode="a", newline="") as file:
@@ -20,3 +21,24 @@ def add_user(first_name, last_name):
         csv_writer.writerow([first_name, last_name])
 
 add_user("Dwayne", "Johnson")
+
+'''
+Ex2:
+For this exercise, you'll be working with a file called users.csv.
+Each row of data consists of two columns: a user's first name, and a user's last name.
+
+Implement the following function:
+
+print_users : prints out all of the first and last names in the users.csv file
+print_users() # None
+# prints to the console:
+# Colt Steele
+'''
+
+def print_users():
+    with open("users.csv", encoding="utf8") as file:
+        csv_reader = DictReader(file)
+        for row in csv_reader:
+            print(row["First Name"] + " " + row["Last Name"])
+
+print_users()
