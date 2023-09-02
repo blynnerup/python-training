68db538d2a1e2ef10fb58c27db9ae9c42f28541a
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-09-02 08:47:52 +0200

    Adds logic to figure out if a user is in csv file

--- CSV/users csv exercises.py
+++ CSV/users csv exercises.py
@@ -13,7 +13,7 @@ add_user("Dwayne", "Johnson") # None
 # Colt,Steele
 # Dwayne,Johnson
 '''
-from csv import writer, DictReader
+from csv import writer, DictReader, reader
 
 def add_user(first_name, last_name):
     with open("users.csv", encoding="utf8", mode="a", newline="") as file:
@@ -24,9 +24,6 @@ add_user("Dwayne", "Johnson")
 
 '''
 Ex2:
-For this exercise, you'll be working with a file called users.csv.
-Each row of data consists of two columns: a user's first name, and a user's last name.
-
 Implement the following function:
 
 print_users : prints out all of the first and last names in the users.csv file
@@ -35,10 +32,35 @@ print_users() # None
 # Colt Steele
 '''
 
-def print_users():
-    with open("users.csv", encoding="utf8") as file:
-        csv_reader = DictReader(file)
-        for row in csv_reader:
-            print(row["First Name"] + " " + row["Last Name"])
+# def print_users():
+#     with open("users.csv", encoding="utf8") as file:
+#         csv_reader = DictReader(file)
+#         for row in csv_reader:
+#             print(row["First Name"] + " " + row["Last Name"])
 
-print_users()
+# print_users()
+
+'''
+Ex3:
+Implement the following function:
+
+find_user : Takes in a first name and a last name and searches for a user with that first and last name in the users.csv file.
+If the user is found, find_user  returns the index where the user is found. 
+Otherwise it returns a message stating that the user wasn't found.
+
+find_user("Colt", "Steele") # 1
+find_user("Alan", "Turing") # 3
+find_user("Not", "Here") # 'Not Here not found.'
+'''
+
+def find_user(first_name, last_name):
+    with open("users.csv", encoding="utf8") as file:
+        csv_reader = reader(file)
+        data = list(csv_reader)
+        user = [first_name, last_name]
+        if user in data:
+            return data.index(user)
+        return f"{first_name} {last_name} not found."
+
+print(find_user('Colt', 'Steele'))
+print(find_user('John', 'Johnson'))
