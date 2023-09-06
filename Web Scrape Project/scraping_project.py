dce7263b037bc4d9a7b263199db54392e6c1163f
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-09-06 07:10:10 +0200

    Adds logic to single out quote

--- Web Scrape Project/scraping_project.py
+++ Web Scrape Project/scraping_project.py
@@ -1,14 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
 def make_url_request():
     url = "http://quotes.toscrape.com"
-
     response = requests.get(url, headers={"Accept": "application/json"})
     soup = BeautifulSoup(response.text, "html.parser")
     quotes = soup.find_all(class_="quote")
-    return quotes
+
+    quotes_data = []
+    for quote in quotes:
+        quotes_data.append({
+            "text": quote.find(class_="text").get_text(),
+            "author": quote.find(class_="author").get_text(),
+            "bio_link": quote.find("a")["href"]
+        })
+    print(random.choice(quotes_data)["text"])
 
 
 
-print(make_url_request())
+make_url_request()
