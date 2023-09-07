c12c2e269b9c1538c4b698e1e12eaf0708fa300e
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-09-07 07:04:44 +0200

    Splits up logic in different functions
    Splits the logic between two functions to seperate responsibility. Also adds more game logic.

--- Web Scrape Project/scraping_project.py
+++ Web Scrape Project/scraping_project.py
@@ -2,11 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
+def play_game():
+    num_guesses = 4
+    selected_quote = make_url_request()
+
+    while num_guesses > 0:
+        if num_guesses == 4:
+            print("Can you guess is the author behind the following quote?")
+            print(selected_quote["text"])
+        if num_guesses == 3:
+            print("Here's a hint:")
+            print(selected_quote["bio_link"])
+        answer = input()
+        if answer == selected_quote["author"]:
+            print("Well done! You guessed it.")
+            num_guesses = 0
+        else:
+            num_guesses -= 1
+
+        if num_guesses == 0:
+            print("Do you want to play again (y/n)?")
+            play_again = input()
+            if play_again == "y":
+                print("Alright, lets go again.")
+                num_guesses = 4
+                selected_quote = make_url_request()
+            else:
+                print("Thanks for playing.")
+                break
+
 def make_url_request():
     url = "http://quotes.toscrape.com"
     response = requests.get(url, headers={"Accept": "application/json"})
     soup = BeautifulSoup(response.text, "html.parser")
     quotes = soup.find_all(class_="quote")
+    num_guesses = 4
 
     quotes_data = []
     for quote in quotes:
@@ -15,8 +45,7 @@ def make_url_request():
             "author": quote.find(class_="author").get_text(),
             "bio_link": quote.find("a")["href"]
         })
-    print(random.choice(quotes_data)["text"])
 
+    return random.choice(quotes_data)
 
-
-make_url_request()
+play_game()
