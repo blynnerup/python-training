19e2e7d0a86d6f4bb40056c95b49ce81c44c0a44
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-09-09 09:11:54 +0200

    Updates the way we scrape quotes
    Moves the logic to generate a random quote. Gets all quotes from the page.
    Adds a sample HTML page for future reference.

--- Web Scrape Project/scraping_project.py
+++ Web Scrape Project/scraping_project.py
@@ -4,17 +4,18 @@ import random
 
 def play_game():
     num_guesses = 4
-    selected_quote = make_url_request()
+    quotes = make_url_request()
+    random_quote = random.choice(quotes)
 
     while num_guesses > 0:
         if num_guesses == 4:
             print("Can you guess is the author behind the following quote?")
-            print(selected_quote["text"])
+            print(random_quote["text"])
         if num_guesses == 3:
             print("Here's a hint:")
-            print(selected_quote["bio_link"])
+            print(random_quote["bio_link"])
         answer = input()
-        if answer == selected_quote["author"]:
+        if answer == random_quote["author"]:
             print("Well done! You guessed it.")
             num_guesses = 0
         else:
@@ -26,26 +27,45 @@ def play_game():
             if play_again == "y":
                 print("Alright, lets go again.")
                 num_guesses = 4
-                selected_quote = make_url_request()
+                random_quote = random.choice(quotes)
             else:
                 print("Thanks for playing.")
                 break
+
+# def get_author_bio():
+#     url = 
 
 def make_url_request():
     url = "http://quotes.toscrape.com"
     response = requests.get(url, headers={"Accept": "application/json"})
     soup = BeautifulSoup(response.text, "html.parser")
     quotes = soup.find_all(class_="quote")
-    num_guesses = 4
+
+    next_page = soup.find(class_="next")
+
+    if next_page:
+        next_page_href = next_page.contents[1]['href']
 
     quotes_data = []
-    for quote in quotes:
-        quotes_data.append({
-            "text": quote.find(class_="text").get_text(),
-            "author": quote.find(class_="author").get_text(),
-            "bio_link": quote.find("a")["href"]
-        })
+    while next_page:
+        for quote in quotes:
+            quotes_data.append({
+                "text": quote.find(class_="text").get_text(),
+                "author": quote.find(class_="author").get_text(),
+                "bio_link": quote.find("a")["href"]
+            })
+        
+        next_url = url + next_page_href
+        response = requests.get(next_url, headers={"Accept": "application/json"})
+        soup = BeautifulSoup(response.text, "html.parser")
+        quotes = soup.find_all(class_="quote")
 
-    return random.choice(quotes_data)
+        next_page = soup.find(class_="next")
+
+        if next_page:
+            next_page_href = next_page.contents[1]['href']
+        
+
+    return quotes_data
 
 play_game()
