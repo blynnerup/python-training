5a4cf10fe146884a65c18ca339f5f53b3ff45853
Brian Lynnerup Pedersen <lynnerup@live.com>
2023-10-07 08:51:49 +0200

    Finishes the 5th part of th exercises.

--- Final Exercises/Ex Part 5/exercises part 5.py
+++ Final Exercises/Ex Part 5/exercises part 5.py
@@ -52,3 +52,45 @@ print(reverse_vowels("Reverse Vowels In A String"))
 print("End reverse_vowels")
 print("----------------------")
 print("Start three_odd_numbers")
+
+"""
+Write a function called three_odd_numbers,
+which accepts a list of numbers and returns True 
+if any three consecutive numbers sum to an odd number.
+"""
+
+def three_odd_numbers(numbers_list):
+   first_three_sum = sum(numbers_list[:2])
+   for i in range(2, len(numbers_list)):
+      first_three_sum += numbers_list[i]
+      if first_three_sum % 2 != 0:
+        return True
+      first_three_sum -= numbers_list[i - 2]
+   else:
+      return False
+
+print(three_odd_numbers([1,2,3,4,5]))
+print(three_odd_numbers([0,-2,4,1,9,12,4,1,0]))
+print(three_odd_numbers([5,2,1]))
+print(three_odd_numbers([1,2,3,3,2]))
+
+print("End three_odd_numbers")
+print("----------------------")
+print("Start mode")
+
+"""
+Write a function called mode.
+This function accepts a list of numbers and returns the most frequent number in the list of numbers.
+You can assume that the mode will be unique.
+"""
+
+def mode(numbers_list):
+    numbers_dict = {}
+    for el in numbers_list:
+      if el not in numbers_dict.keys():
+         numbers_dict[el] =  1
+      else:
+         numbers_dict[el] += 1
+    return max(numbers_dict, key=numbers_dict.get)
+
+print(mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4]))
